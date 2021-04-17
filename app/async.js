a133f16ca9f1d1411b1aeaff86b2f55924c8229e
Bruno Martins <bruno.martins217@gmail.com>
2021-04-16 23:41:13 -0300

    Friday commit

--- app/async.js
+++ app/async.js
@@ -1,12 +1,18 @@
-
 exports = typeof window === 'undefined' ? global : window;
-
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise((resolve, reject) => {
+      if(typeof value === 'number') {
+        reject(new Error('Invalid url'))
+      } else {
+        resolve(value)
+      }
+    })
   },
-
+  
   manipulateRemoteData: function(url) {
-
+    const data = $.ajax(url).then((response) => response.people.map((element) => element.name).sort())
+    return data
   }
-};
+  // https://api.jquery.com/jquery.ajax/
+}
--- app/functions.js
+++ app/functions.js
@@ -60,6 +60,6 @@ exports.functionsAnswers = {
   },
 
   curryIt: function(fn) {
-
+    
   }
 };
